b3934b79ecd71cdf77d0f5e2717d41dcf4b1daf2
DvDChC <david.chacaton@gmail.com>
2017-02-26 21:46:00 +0100

    Delete user.js

--- public/js/user.js
+++
@@ -1,8 +0,0 @@
-
-var user;
-var socket = io.connect('http://localhost:8080');
-$('#btnConnexion').click(function() {
-	socket.emit('request', {command : 'identify', data: {login: $('#login').val()}
-	});
-	user = $('#login').val();
-})
